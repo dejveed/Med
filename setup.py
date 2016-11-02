a3605e6bff3676b48ceda81639197a5b9dad1d29
dejveed <dawid.pacia@homersoft.com>
2016-11-02 19:53:01 +0100

    First Files

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name='Medius Test',
+    version='1.0',
+    description='Sample test for website',
+    author='Dawid Pacia',
+    author_email='paciadawid@gmail.com',
+    url='https://github.com/dejveed/MediusTest',
+    packages=['selenium'],
+    classifiers=[
+        # Indicate who your project is intended for
+        "Intended Audience :: Developers & Recruiters : ) ",
+        "Topic :: Software Development",
+
+        "Operating System :: OS Independent",
+
+        #Supported python versions
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+    ]
+)
