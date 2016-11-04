5cc447d08d6f4db9a0bbae54bddda326a61948b2
dejveed <dawid.pacia@homersoft.com>
2016-11-04 14:44:28 +0100

    PoP

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-pip3 install selenium==2.53.6
+##Requirements
+
+- Python 3.4
+- Selenium ver. 2.x.x (to install use command _pip3 install selenium==2.53.6_)
+- [Selenium webriver](http://chromedriver.storage.googleapis.com/index.html?path=2.25) (it's already inside data folder)
---
+++ tests/medTest.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+import os
+from data import common
+from pop.mainPage import MainPage
+import unittest
+import time
+
+
+class MedTest(unittest.TestCase):
+
+    def setUp(self):
+        configFile = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'data', 'credentials.json')
+        form = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'data', 'expenceInfoiceDataForm.json')
+        self.credentials = common.loadJson(configFile)
+        self.form = common.loadJson(form)
+        self.greenNotificationWindow = '223, 240, 216'
+
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option("excludeSwitches", ["ignore-certificate-errors"])
+        chromedriver = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'data', 'chromedriver.exe')
+
+        self.driver = webdriver.Chrome(chromedriver, chrome_options=options)
+        self.mainPage = MainPage(self.driver)
+        self.mainPage.navigateToPage('https://cloud.mediusflow.com/rndQA/LocalAccount/')
+
+    def tearDown(self):
+        time.sleep(5)
+        self.driver.quit()
+
+    def test01(self):
+        self.mainPage.loginToPage(self.credentials['login'], self.credentials['password'])
+        self.mainPage.navigateToTab()
+        self.mainPage.fillForm(self.form)
+        currentWindowColor = self.mainPage.getNotificationColorValue()
+        assert self.greenNotificationWindow in currentWindowColor, \
+            "The color of notification window is '{0}' while it should be '{1}'".format(currentWindowColor,
+                                                                                        self.greenNotificationWindow)
